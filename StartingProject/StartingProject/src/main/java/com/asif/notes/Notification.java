f08998ad6fb485b9efe14c6cd8c6ba5ef96f452f
Asif Khan <23imak@gmail.com>
2015-01-14 13:28:16 +0530

    Updated listView

---
+++ StartingProject/StartingProject/src/main/java/com/asif/notes/Notification.java
@@ -0,0 +1,18 @@
+package com.asif.notes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Irfan on 10-01-2015.
+ */
+
+public class Notification extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+
+
+    }
+}
---
+++ StartingProject/StartingProject/src/main/java/com/asif/notes/Profile.java
@@ -0,0 +1,22 @@
+package com.asif.notes;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.widget.AbsListView;
+
+/**
+ * Created by asif on 10-01-2015.
+ */
+public class Profile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+
+
+}
+
